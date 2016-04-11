20fba6ba2fe2f855fcde89d963ebc1f740ae1985
Dan Jones <good.evil.genius@gmail.com>
2016-04-11 14:59:44 -0500

    Support oEmbeds now

--- Makefile
+++ Makefile
@@ -5,6 +5,6 @@ build:
 	/usr/local/bin/jekyll build
 
 deploy: build
-	git add -A _posts
+	git add -A _posts _data
 	git commit -m "Build for `date`"
 	git push github gh-pages
--- _scripts/build.py
+++ _scripts/build.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import yaml, string, os, codecs
+import yaml, string, os, codecs, urllib, json, fnmatch
 
 try:
     import droplogger
@@ -18,11 +18,37 @@ config['end'] = droplogger.parse_date('now')
 config['files'] = droplogger.get_files(**config)
 entries = droplogger.read_files(**config)
 
+oembed_file = '_data/oembed.yml'
+
+fp = open(oembed_file,'r')
+oembed = yaml.load(fp)
+fp.close()
+
 for e in entries['pile']:
     e['layout'] = "post"
     if "url" in e:
         if e is None: del e["url"]
         else: e["embed_url"] = e.pop("url")
+
+    if "embed_url" in e:
+        embeds = [ o for o in oembed['urls'] if o['url'] == e["embed_url"] ]
+        if len(embeds) == 0:
+            providers = [ p for p in oembed['providers'] if "schemes" in p['endpoints'][0] and len([ s for s in p['endpoints'][0]['schemes'] if fnmatch.fnmatch(e["embed_url"], s) ]) ]
+            if len(providers) > 0:
+                ep = providers[0]['endpoints'][0]['url']
+                oembed_url = ep + '?' + urllib.urlencode({"format":"json","url":e['embed_url']})
+                try:
+                    embed = json.load(urllib.urlopen(oembed_url))
+                    embed['url'] = e["embed_url"]
+                    oembed["urls"].append(embed)
+                    fp = open(oembed_file,'w')
+                    yaml.safe_dump(oembed, fp, encoding='utf-8')
+                    fp.close()
+                except Error:
+                    embed = None
+                embeds = [ embed ]
+        if embeds[0] is not None:
+            e["embed_html"] = embeds[0]['html']
 
     slug = ''.join(c for c in e['title'].replace(' ','_') if c in ("-_.%s%s" % (string.ascii_letters, string.digits)))
     filename = "%s-%s-%s-%s.md" % (e['date'].year, e['date'].month, e['date'].day, slug)
@@ -31,6 +57,9 @@ for e in entries['pile']:
     fp.write('---\n')
     yaml.safe_dump(e, fp, encoding='utf-8')
     fp.write('---\n')
+    if "embed_html" in e:
+        fp.write(e['embed_html'])
+        fp.write('\n\n')
     if "text" in e and e['text'] is not None:
         fp.write(e['text'])
     elif "note" in e and e['note'] is not None:
