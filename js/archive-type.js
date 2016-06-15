3b654930792df98551459032ec721bbadc98375a
Dan Jones <good.evil.genius@gmail.com>
2016-06-14 21:16:44 -0500

    Add random links for tag and type archive pages
    More to come soon.

---
+++ js/archive-type.js
@@ -0,0 +1,39 @@
+---
+---
+var head = document.querySelector('#main > h1');
+var type = head.dataset.archive;
+
+var rand = document.createElement('a');
+rand.setAttribute('rel','random');
+rand.setAttribute('href', '#');
+rand.className = 'post-random';
+var img = document.createElement('img');
+rand.appendChild(img);
+
+if (type == "tag" || type == "type") head.appendChild(rand);
+
+rand.addEventListener("click", function() {
+	var loadNewPage = function() {
+		var newposts = window.posts.filter(function(p){
+			switch (type) {
+			case "tag":
+				return p.tags.includes(head.dataset.archiveTag);
+				break;
+			case "type":
+				return p.type == head.dataset.archiveType;
+				break;
+			default:
+				return false;
+			}
+		});
+		var pn = Math.floor(Math.random() * newposts.length);
+		window.location = newposts[pn].url;
+	};
+
+	var script = document.createElement('script');
+	script.setAttribute('src', {{site.baseurl|append:'/js/posts.js'|jsonify}});
+	script.addEventListener("readystatechange", loadNewPage);
+	script.addEventListener("load", loadNewPage);
+	
+	document.body.appendChild(script);
+});
