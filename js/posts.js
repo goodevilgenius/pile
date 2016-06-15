e6ad736c050621a875bc2b92a8816367a1b6302e
Dan Jones <good.evil.genius@gmail.com>
2016-06-14 20:44:03 -0500

    Add date and timestamp to posts.js

--- js/posts.js
+++ js/posts.js
@@ -6,6 +6,8 @@ posts.push({
 	"title":{{post.title|jsonify}},
 	"url":{{post.url|prepend:site.baseurl|prepend:site.url|jsonify}},
 	"type":{{post.type|jsonify}},
-	"tags":{{post.tags|jsonify}}
+	"tags":{{post.tags|jsonify}},
+	"date":{{post.date|jsonify}},
+	"timestamp":{{post.timestamp|jsonify}}
 });
 {% endfor %}
