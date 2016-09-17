78fc47c7e505baa5b9feca19232799343d867049
Dan Jones <dan@danielrayjones.com>
2016-09-17 17:07:16 -0500

    [Add] My little thing for stuff

---
+++ c9f.js
@@ -0,0 +1,8 @@
+var s=document.createElement('link');
+s.setAttribute('href','https://fonts.googleapis.com/css?family=Inconsolata');
+s.setAttribute('rel','stylesheet');
+document.getElementsByTagName('body')[0].appendChild(s);
+var s=document.createElement('link');
+s.setAttribute('href','https://fonts.googleapis.com/css?family=Source+Code+Pro');
+s.setAttribute('rel','stylesheet');
+document.getElementsByTagName('body')[0].appendChild(s);
